67ea808c2bd771724f406fb0687a549f2dfe9f5e
chunyukuo88 <alexgochenour@gmail.com>
2019-12-24 10:44:08 -0500

    Finished a second, super-simple JS kata. Learned more about the module.exports statement, particularly the importance of including versus excluding the () in statements like module.exports = someFunction;

--- __tests__/isograms.spec.js
+++ __tests__/isograms.spec.js
@@ -1,9 +1,31 @@
-const forEmptyString = require("../src/isograms");
+// const forEmptyString = require("../src/isograms");
 
-describe("Determines if a word is an isogram",  ()=> {
+// describe("Determines if a word is an isogram",  ()=> {
 
-    test("Should account for empty string", () => {
-        const input = "";
-        expect(forEmptyString(input)).toEqual(true);
+//     test("Should account for empty string", () => {
+//         const input = "";
+//         expect(forEmptyString(input)).toEqual(true);
+//     })
+// })
+
+// const sortChars = require("../src/isograms");
+
+// describe("Can sort characters of a string", () => {
+
+//     test("Can sort the chars of a string", () => {
+//         const input = "abcdefgaabb";
+//         expect(sortChars(input)).toEqual("aaabbbcdefg");
+//     })
+// })
+
+const isIsogram = require("../src/isograms");
+
+describe("Can tell you if the word is an isogram or not", () => {
+
+    test("Returns true if it is an isogram, false if otherwise", ()=> {
+        const input = "abcdefga";
+        
+        expect(isIsogram(input)).toEqual(false);
     })
+
 })
--- src/isograms.js
+++ src/isograms.js
@@ -1,8 +1,36 @@
-function forEmptyString(string){
-    if (string == "")
-        return true;
-    else
-        return false;
+// function forEmptyString(string){
+//     if (string == "")
+//         return true;
+//     else
+//         return false;
+// }
+
+// module.exports = forEmptyString;
+
+
+function sortChars(word){
+    let arraySplit = word.split('');
+    let arraySort = arraySplit.sort();
+    let arrayJoin = arraySplit.join('');
+        return arrayJoin;
 }
 
-module.exports = forEmptyString;
+function isIsogram(word) {
+    let x = 0;
+    const sortedWord = word.toLowerCase().split('').sort();
+    for (let i = 1 ; i < sortedWord.length ; i++) {
+        if (sortedWord[i] == sortedWord[i-1] || sortedWord[i] == sortedWord[i+1]){
+            x += 1;
+        }
+    }
+
+    if (x == 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+// module.exports = sortChars;
+module.exports = isIsogram;
