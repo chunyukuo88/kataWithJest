abaa3db7a97605a017cd654447e8008a5ef54a52
chunyukuo88 <alexgochenour@gmail.com>
2020-02-06 12:21:24 -0500

    The tests in VS Code work, but Codewars.com objects. I think I need to account for leading zeroes.

---
+++ __tests__/stringIncrementor.spec.js
@@ -0,0 +1,51 @@
+const { containsLettersAndNums, 
+        incrementString,
+        produceIntegerComponent } = require('../src/stringIncrementor');
+
+describe('Invalid cases', () => {
+
+    it('returns 1 one if the string is empty.', ()=> {
+        const input = '';
+        const result = incrementString(input);
+        const expectedResult = '1';
+        expect(result).toBe(expectedResult);
+    })
+})
+
+describe('Valid cases', () => {
+
+    it('increments by one if the string consists of a number.', ()=>{
+        const input = '100';
+        const result = incrementString(input);
+        expect(result).toBe('101');
+    })
+
+    it('appends a 1 to a string with no numbers in it.', ()=>{
+        const input = 'Word';
+        const result = incrementString(input);
+        expect(result).toBe('Word1');
+    })
+
+    it('increments a string that contains both letters & numbers', ()=>{
+        const input = 'Word99';
+        const result = incrementString(input);
+        expect(result).toBe('Word100');
+    })
+
+})
+
+describe('Can produce separate strings if input contains both letters and numbers', ()=> {
+
+    it('returns a boolean evaluating if the string contains numbers AND letters', ()=> {
+        const input = 'Word99';
+        const result = containsLettersAndNums(input);
+        expect(result).toEqual(true);
+    })
+
+    it('can return the number component of the string as an integer', ()=>{
+        const input = 'Word99';
+        const result = produceIntegerComponent(input);
+        expect(result).toEqual(99);
+    })
+
+})
---
+++ src/stringIncrementor.js
@@ -0,0 +1,58 @@
+// https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/javascript
+
+function containsLettersAndNums(input){
+    let inputAsArray = input.split('');
+    let result = false;
+    for (let index = 0; index < inputAsArray.length; index++) {
+        const element = inputAsArray[index];
+        if (parseInt(element)*0 === 0){
+            result = true;
+            break;
+        }
+    } 
+    return result;
+}
+
+function produceIntegerComponent(input){
+    let inputAsArray = input.split('');
+    let result = '';
+    for (let index = 0; index < inputAsArray.length; index++) {
+        const element = inputAsArray[index];
+        if (parseInt(element)*0 === 0){
+            result += element;
+        }
+    } 
+    return parseInt(result);
+}
+
+
+function incrementString(inputString){
+    if (inputString === '')
+        return '1';
+    else if (Number.isInteger(parseInt(inputString, 10)))
+        return (parseInt(inputString, 10)+1).toString();
+    else if (containsLettersAndNums(inputString))
+        return lettersAndNumbersIncrement(inputString);
+    else
+        return inputString + '1';
+}
+
+function lettersAndNumbersIncrement(inputString){
+    let inputArray = inputString.split('');
+    let letterComponent = '';
+    let numberComponent = '';
+    for (let index = 0; index < inputArray.length; index++) {
+        const element = inputArray[index];
+        if(element === '0' || parseInt(element, 10)*0 === 0)
+            numberComponent += element;
+        else
+            letterComponent += element;
+    }
+    numberComponent = parseInt(numberComponent) + 1;
+
+    return letterComponent + numberComponent.toString();
+}
+
+module.exports = {incrementString, 
+                  containsLettersAndNums,
+                  produceIntegerComponent};
