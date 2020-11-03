7af480318d7a64e8bca1af85b2201fd5acb88cc3
chunyukuo88 <alexgochenour@gmail.com>
2020-11-02 23:18:44 -0500

    Scrapped the previous approach and switched to use the hasOwnProperty JavaScript method.

--- kata-with-jest/src/peteTheBaker.js
+++ kata-with-jest/src/peteTheBaker.js
@@ -1,13 +1,17 @@
 //Source:
 //https://www.codewars.com/kata/525c65e51bf619685c000059/train/javascript
 
-export function mapFieldsToArrayOfObjects(recipeObject){
-  const result = [];
-  for (const [ingredient, amount] of Object.entries(recipeObject)){
-    const ingredientValuePair = {};
-    ingredientValuePair[ingredient] = amount;
-    result.push(ingredientValuePair);
-  }
-  console.log(result);
-  return result;
-}
+export function canMakeAtLeastOne(recipe, available) {
+  let hasEnoughForOne = true;
+  for (const ingredient in recipe) {
+    if (!available.hasOwnProperty(ingredient)) {
+      hasEnoughForOne = false;
+      break;
+    }
+    if (available[ingredient] < recipe[ingredient]) {
+      hasEnoughForOne = false;
+      break;
+    };
+  };
+  return hasEnoughForOne;
+};
--- kata-with-jest/src/peteTheBaker.test.js
+++ kata-with-jest/src/peteTheBaker.test.js
@@ -1,15 +1,56 @@
 import * as functions from './peteTheBaker';
 
 describe('peteTheBaker.js', ()=>{
-  describe('mapFieldsToArrayOfObjects()', ()=>{
-    test('It maps the recipe ingredients as ingredient-value pairs.', ()=>{
-      const recipeObject = {
-        a: 'a',
-        b: 'b'
-      };
-      const result = functions.mapFieldsToArrayOfObjects(recipeObject);
-      const expectedResult = [{a: 'a'}, {b: 'b'}];
-      expect(result).toEqual(expectedResult);
+  describe('checkForIngredientAvailability()', ()=>{
+    describe('WHEN: Pete has the ingredients to make at least one batch of the recipe,', ()=>{
+      test('THEN: It returns the boolean true, meaning the recipe can be made.', ()=>{
+        const recipe = {
+          a: 1,
+          b: 2,
+          c: 3
+        };
+        const available = {
+          a: 1,
+          b: 2,
+          c: 300
+        };
+        const result = functions.canMakeAtLeastOne(recipe, available);
+        const expectedResult = true;
+        expect(result).toEqual(expectedResult);
+      });
+    });
+    describe('WHEN: Pete has an ingredient but in insufficient quantity,', ()=>{
+      test('THEN: It returns the boolean false, meaning the recipe can"t be made.', ()=>{
+        const recipe = {
+          a: 1,
+          b: 2,
+          c: 3
+        };
+        const available = {
+          a: 1,
+          b: 2,
+          c: 0
+        };
+        const result = functions.canMakeAtLeastOne(recipe, available);
+        const expectedResult = false;
+        expect(result).toEqual(expectedResult);
+      });
+    });
+    describe('WHEN: Pete is missing an ingredient,', ()=>{
+      test('THEN: It returns the boolean false, meaning the recipe can"t be made.', ()=>{
+        const recipe = {
+          a: 1,
+          b: 2,
+          c: 3
+        };
+        const available = {
+          a: 1,
+          b: 2
+        };
+        const result = functions.canMakeAtLeastOne(recipe, available);
+        const expectedResult = false;
+        expect(result).toEqual(expectedResult);
+      });
     });
   });
 });
