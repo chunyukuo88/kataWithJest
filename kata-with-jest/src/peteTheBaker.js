0a799089e743509bcf0a0bcc9cc7906323e90e53
chunyukuo88 <alexgochenour@gmail.com>
2020-11-03 21:55:29 -0500

    Finished Pete The Baker kata.

--- kata-with-jest/src/peteTheBaker.js
+++ kata-with-jest/src/peteTheBaker.js
@@ -1,6 +1,27 @@
 //Source:
 //https://www.codewars.com/kata/525c65e51bf619685c000059/train/javascript
 
+/**
+ Target function:
+
+ * */
+
+export function cakes(recipe, available) {
+  return (!canMakeAtLeastOne(recipe, available))
+    ? 0
+    : compareAmounts(recipe, available);
+}
+
+export function compareAmounts(recipe, available) {
+  const cakesInTermsOfIngredients = [];
+  Object.keys(recipe).forEach(ingredient => {
+    const cakesPerIngredient = Math.floor((available[ingredient] / recipe[ingredient]));
+    cakesInTermsOfIngredients.push(cakesPerIngredient);
+  });
+  const result = Math.min(...cakesInTermsOfIngredients);
+  return result;
+}
+
 export function canMakeAtLeastOne(recipe, available) {
   let hasEnoughForOne = true;
   for (const ingredient in recipe) {
--- kata-with-jest/src/peteTheBaker.test.js
+++ kata-with-jest/src/peteTheBaker.test.js
@@ -1,19 +1,44 @@
 import * as functions from './peteTheBaker';
 
 describe('peteTheBaker.js', ()=>{
+  describe('cakes()', ()=>{
+    describe('WHEN: Pete does not have enough ingredients to make one cake', ()=> {
+      test('THEN: It returns zero.', ()=>{
+        const recipe =    { a: 1, b: 2,  c: 3 };
+        const available = { a: 1, b: 2 };
+        const result = functions.cakes(recipe, available);
+        const expectedResult = 0;
+        expect(result).toEqual(expectedResult);
+      });
+    });
+  });
+  describe('cakes()', ()=>{
+    describe('WHEN: Pete has enough ingredients to make one cake', ()=> {
+      test('THEN: It returns the number of cakes he can make.', ()=>{
+        const recipe =    { a: 1, b: 1, c: 3 };
+        const available = { a: 3, b: 3, c: 6 };
+        const result = functions.cakes(recipe, available);
+        const expectedResult = 2;
+        expect(result).toEqual(expectedResult);
+      });
+    });
+  });
+  describe('compareAmounts()', ()=>{
+    describe('WHEN: Pete has enough ingredients to make one cake', ()=>{
+      test('THEN: It returns the number of cakes he can make', ()=>{
+        const recipe =    { a: 1, b: 1, c: 1};
+        const available = { a: 2, b: 2, c: 3};
+        const result = functions.compareAmounts(recipe, available);
+        const expectedResult = 2;
+        expect(result).toEqual(expectedResult);
+      });
+    });
+  });
   describe('checkForIngredientAvailability()', ()=>{
     describe('WHEN: Pete has the ingredients to make at least one batch of the recipe,', ()=>{
       test('THEN: It returns the boolean true, meaning the recipe can be made.', ()=>{
-        const recipe = {
-          a: 1,
-          b: 2,
-          c: 3
-        };
-        const available = {
-          a: 1,
-          b: 2,
-          c: 300
-        };
+        const recipe =    { a: 1, b: 2, c: 3 };
+        const available = { a: 1, b: 2, c: 300 };
         const result = functions.canMakeAtLeastOne(recipe, available);
         const expectedResult = true;
         expect(result).toEqual(expectedResult);
@@ -21,16 +46,8 @@ describe('peteTheBaker.js', ()=>{
     });
     describe('WHEN: Pete has an ingredient but in insufficient quantity,', ()=>{
       test('THEN: It returns the boolean false, meaning the recipe can"t be made.', ()=>{
-        const recipe = {
-          a: 1,
-          b: 2,
-          c: 3
-        };
-        const available = {
-          a: 1,
-          b: 2,
-          c: 0
-        };
+        const recipe =    { a: 1, b: 2, c: 3 };
+        const available = { a: 1, b: 2, c: 0 };
         const result = functions.canMakeAtLeastOne(recipe, available);
         const expectedResult = false;
         expect(result).toEqual(expectedResult);
@@ -38,15 +55,8 @@ describe('peteTheBaker.js', ()=>{
     });
     describe('WHEN: Pete is missing an ingredient,', ()=>{
       test('THEN: It returns the boolean false, meaning the recipe can"t be made.', ()=>{
-        const recipe = {
-          a: 1,
-          b: 2,
-          c: 3
-        };
-        const available = {
-          a: 1,
-          b: 2
-        };
+        const recipe =    { a: 1, b: 2, c: 3 };
+        const available = { a: 1, b: 2 };
         const result = functions.canMakeAtLeastOne(recipe, available);
         const expectedResult = false;
         expect(result).toEqual(expectedResult);
