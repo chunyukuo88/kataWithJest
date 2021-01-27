9991063f42fbba8addf9440adf83862d24a0a5cc
chunyukuo88 <alexgochenour@gmail.com>
2021-01-27 15:38:45 -0500

    Added a pig latin converter with some tests. Next: Figure out the RegEx for testing non-word inputs.

---
+++ kata-with-jest/src/pigLatin.js
@@ -0,0 +1,33 @@
+export const pigLatinConverter = input => {
+    const inputIsValid = checkInputValidity(input);
+    return (inputIsValid)
+        ? pigLatinize(input)
+        : invalidInputMessage;
+};
+
+const checkInputValidity = (input) => {
+    if (typeof input !== 'string' || input === undefined || input === null) return false;
+    if (!inputIsWord(input)) return false;
+    return true;
+};
+
+const inputIsWord = (input) => {
+    const userInputAsArray = input.split('');
+    let inputIsWord = true;
+    userInputAsArray.forEach(char => {
+        if (char !== /[a-z]/i) inputIsWord = false;
+    })
+    return inputIsWord;
+};
+
+const pigLatinize = (input) => {
+    const userInputAsArray = input.split('');
+    //Add functionality for vowels, etc.
+    const consonantPlusAy = userInputAsArray[0] + 'ay';
+    userInputAsArray.splice(0, 1);
+    userInputAsArray.push(consonantPlusAy);
+    const result = userInputAsArray.join('');
+    return result;
+};
+
+export const invalidInputMessage = 'Please enter an ordinary word consisting of Latin letters only.'
---
+++ kata-with-jest/src/pigLatin.test.js
@@ -0,0 +1,60 @@
+import { pigLatinConverter, invalidInputMessage } from './pigLatin';
+
+describe('pigLatin.js', ()=>{
+    describe('pigLatinConverter()', ()=>{
+        describe('WHEN: Given a valid string,', ()=>{
+            test('AND: The string starts with a consonant,', ()=>{
+                const input = 'word';
+                const result = pigLatinConverter(input);
+                const expectedResult = 'ordway';
+                expect(result).toBe(expectedResult);
+            });
+            test('AND: The string starts with two consecutive consonants,', ()=>{
+                // const input = 'cloud';
+                // const result = pigLatinConverter(input);
+                // const expectedResult = 'oudclay';
+                // expect(result).toBe(expectedResult);
+            });
+            test('AND: The string starts with three consecutive consonants,', ()=>{
+
+            });
+            test('AND: The string starts with a vowel,', ()=>{
+
+            });
+        });
+        describe('WHEN: Given an invalid input,', ()=>{
+            describe('AND: And the input is not a string,', ()=>{
+                test('THEN: It returns the invalid input error message,', ()=>{
+                    const input = {};
+                    const result = pigLatinConverter(input);
+                    const expectedResult = invalidInputMessage;
+                    expect(result).toBe(expectedResult);
+                });
+            });
+            describe('WHEN: And the input a string but does not consist entirely of Latin letters,', ()=>{
+                test('THEN: It returns the invalid input error message,', ()=>{
+                    const input = '===';
+                    const result = pigLatinConverter(input);
+                    const expectedResult = invalidInputMessage;
+                    expect(result).toBe(expectedResult);
+                });
+            });
+            describe('WHEN: And the input is null,', ()=>{
+                test('THEN: It returns the invalid input error message,', ()=>{
+                    const input = null;
+                    const result = pigLatinConverter(input);
+                    const expectedResult = invalidInputMessage;
+                    expect(result).toBe(expectedResult);
+                });
+            });
+            describe('WHEN: And the input is undefined,', ()=>{
+                test('THEN: It returns the invalid input error message,', ()=>{
+                    const input = undefined;
+                    const result = pigLatinConverter(input);
+                    const expectedResult = invalidInputMessage;
+                    expect(result).toBe(expectedResult);
+                });
+            });
+        });
+    });
+});
