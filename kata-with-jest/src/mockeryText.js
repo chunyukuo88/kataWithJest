3c972ef3256b47bb83724f3932174180ae523308
Alex Gochenour <alexgochenour@gmail.com>
2021-03-22 16:43:36 -0400

    Test to account for sentences. Passing tests, refactoring.

--- kata-with-jest/src/mockeryText.js
+++ kata-with-jest/src/mockeryText.js
@@ -1,18 +1,27 @@
 // Produces mockery text, the first letter capitalized.
+// Example: 'BeN ShApiRo'S FaCtS & LoGiC'
 // The letters L and I are never capitalized unless they are the first letter.
 
-export default function convertToMockeryText (inputString) {
-  const inputAsArray = capitalizeFirstLetterOfInput(inputString);
+export default function convertToMockeryText(inputString) {
+  const inputAsArrayOfWords = inputString.split(' ');
+  const output = [];
+  inputAsArrayOfWords.forEach(word => processIndividualWord(word, output));
+  return output.join(' ');
+};
+
+const processIndividualWord = (word, output) => {
+  const inputAsArray = capitalizeFirstLetterOfInput(word);
   const mockeryOutputAsArray = prepOutputAsArray(inputAsArray);
   capitalizeEveryOtherLetter(inputAsArray, mockeryOutputAsArray);
-  return mockeryOutputAsArray.join('');
-}
+  const modifiedWord = mockeryOutputAsArray.join('');
+  output.push(modifiedWord);
+};
 
 const prepOutputAsArray = (inputAsArray) => {
   const mockeryOutputAsArray = [];
   mockeryOutputAsArray.push(inputAsArray[0]);
   return mockeryOutputAsArray;
-}
+};
 
 const capitalizeEveryOtherLetter = (inputAsArray, mockeryOutputAsArray) => {
   for (let i = 1; i < inputAsArray.length; i++) {
@@ -31,4 +40,4 @@ const capitalizeFirstLetterOfInput = (inputString) => {
   inputAsArray.splice(0, 0, inputAsArray[0].toUpperCase());
   inputAsArray.splice(1, 1);
   return inputAsArray;
-}
+};
--- kata-with-jest/src/mockeryText.test.js
+++ kata-with-jest/src/mockeryText.test.js
@@ -2,11 +2,19 @@ import convertToMockeryText from './mockeryText';
 
 describe('mockeryText.js', ()=>{
   describe('mockeryText()', ()=>{
-    describe('GIVEN: A valid string, ', ()=>{
+    describe('GIVEN: A valid string consisting of a single word, ', ()=>{
       it('THEN: It returns the string in mockery text, ', ()=>{
         const input = 'rabbit';
         const result = convertToMockeryText(input);
         const expectedResult = 'RaBbIt';
+        expect(result).toEqual(expectedResult);
+      });
+    });
+    describe('GIVEN: A valid string with spaces, ', ()=>{
+      it('THEN: It returns the string in mockery text, ignoring the spaces', ()=>{
+        const input = 'facts and logic';
+        const result = convertToMockeryText(input);
+        const expectedResult = 'FaCtS AnD LoGiC';
         expect(result).toEqual(expectedResult);
       });
     });
