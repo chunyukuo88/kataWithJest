16f3c626e73e431482e976a3da9c1e79689492f8
Alex Gochenour <alexgochenour@gmail.com>
2021-07-16 14:51:51 -0400

    Unique sort

--- kata-with-jest/src/uniqueSort.js
+++ kata-with-jest/src/uniqueSort.js
@@ -1,23 +1,10 @@
 export const uniqueSort = (arrayOfIntegers) => {
-  const breadcrumbs = [];
-  arrayOfIntegers.forEach(int => !breadcrumbs[int] && breadcrumbs.push(int));
-  return breadcrumbs.sort((a, b) => a - b);
+  const cache = {};
+  return arrayOfIntegers.map(int => {
+    if (!(int in cache)) {
+      cache[int] = true;
+      return int;
+    }
+  }).sort();
 };
 
-const times10 = (integer) => integer * 10;
-
-const cache = {};
-
-const memoTimes10 = (integer) => {
-  if (!cache[integer]) {
-    const result = times10(integer);
-    cache[integer] = result;
-    console.log(`${integer} times 10 = ${result}`);
-    return;
-  }
-  console.log(`${integer} times 10 = ${cache[integer]}. This result has been retrieved from cache.`);
-};
-
-memoTimes10(10);
-memoTimes10(9);
-memoTimes10(10);
