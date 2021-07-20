3b92fadba888897414815df403b3b2e519328826
Alex Gochenour <alexgochenour@gmail.com>
2021-07-20 10:59:31 -0400

    Refactored.

--- kata-with-jest/src/recursiveFactorial.js
+++ kata-with-jest/src/recursiveFactorial.js
@@ -1,24 +1,23 @@
-const givenNumbers = {};
+const cache = {};
 
 export const recursiveFactorial = (number) => {
-  if (hasBeenMemoized(number)) return givenNumbers[number];
-  let result = 1, countdown = number;
+  if (hasBeenMemoized(number)) return cache[number];
+  let result = 1;
 
   function recurse(countdown){
     if (countdown > 1) {
       result = result * countdown;
-      countdown--;
-      recurse(countdown);
+      recurse(countdown - 1);
     }
   }
-  recurse(countdown);
-  givenNumbers[number] = result;
+  recurse(number);
+  cache[number] = result;
   return result;
 };
 
 const hasBeenMemoized = (number) => {
-  if (givenNumbers[number]) {
-    console.log(`${givenNumbers[number]} has been memoized.`)
+  if (cache[number]) {
+    console.log(`${cache[number]} has been memoized.`)
     return true;
   }
 };
