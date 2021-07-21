5b90d8e8aa1411fef6ad82f7c152882a6a39c912
Alex Gochenour <alexgochenour@gmail.com>
2021-07-21 00:03:27 -0400

    How I loathe bubble sort. The key was adding a temporary value on line 19.

---
+++ kata-with-jest/src/algorithmsCourseFEM/linearBinaryBubble.js
@@ -0,0 +1,27 @@
+const { log } = console;
+import _ from 'lodash';
+
+export const linearSearch = (list, number) => {
+  let result = -1;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === number) {
+      result = i;
+      break;
+    }
+  };
+  return result;
+};
+
+export const bubbleSort = (list) => {
+  for (let i = 0; i < list.length; i++) {
+    for (let j = 0; j < list.length; j++) {
+      if (list[j] > list[j + 1]) {
+        const temp = list[j];
+        list[j] = list[j+1];
+        list[j + 1] = temp;
+      }
+    };
+  };
+  log(list)
+  return list;
+};
---
+++ kata-with-jest/src/algorithmsCourseFEM/linearBinaryBubble.test.js
@@ -0,0 +1,48 @@
+import { linearSearch, bubbleSort } from './linearBinaryBubble';
+
+describe('linearBinaryBubble.js', ()=>{
+  describe('linearSearch()', ()=>{
+    describe('WHEN: Given a list and a number that is contained in that list, ', ()=>{
+      it('THEN: It returns the index of the number in the array', ()=>{
+        const [list, number] = [[2,6,7,90,103], 90];
+        const expectedResult = 3;
+
+        const result = linearSearch(list, number);
+
+        expect(result).toEqual(expectedResult);
+      });
+    });
+    describe('WHEN: Given a list and a number that is NOT contained in that list, ', ()=>{
+      it('THEN: It returns -1', ()=>{
+        const [list, number] = [[2,6,7,90,103], 900000000000];
+        const expectedResult = -1;
+
+        const result = linearSearch(list, number);
+
+        expect(result).toEqual(expectedResult);
+      });
+    });
+    describe('WHEN: Given a list and a number that is contained in that list multiple times, ', ()=>{
+      it('THEN: It returns the index of the first occurrence of the number in the array', ()=>{
+        const [list, number] = [[2,6,7,90,90,90,103], 90];
+        const expectedResult = 3;
+
+        const result = linearSearch(list, number);
+
+        expect(result).toEqual(expectedResult);
+      });
+    });
+  });
+  describe('bubbleSort()', ()=>{
+    describe('WHEN: Given a list of numbers, ', ()=>{
+      it('THEN: It sorts them.', ()=>{
+        const originalList   = [ 9, 2, 5, 6, 4, 7 ];
+        const expectedResult = [ 2, 4, 5, 6, 7, 9 ];
+
+        const result = bubbleSort(originalList);
+
+        expect(result).toEqual(expectedResult);
+      });
+    });
+  });
+});
