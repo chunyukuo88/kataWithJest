44a17bee94ce8f0d280557f67135b3e077dd24a1
Alex Gochenour <alexgochenour@gmail.com>
2021-08-16 19:15:33 -0400

    WIP

--- kata-with-jest/src/Academind/binarySearch.js
+++ kata-with-jest/src/Academind/binarySearch.js
@@ -1,35 +1,16 @@
-let iteration = 0;
 export const binarySearch = (array, targetNumber) => {
-  iteration++;
-  const middleIndex = Math.round(array.length / 2) - 1;
+  let startIndex = 0;
+  let endIndex = array.length - 1;
+  const middleIndex = Math.floor((endIndex - startIndex) / 2);
+
   if (array[middleIndex] === targetNumber) return middleIndex;
-  const { firstHalf, secondHalf } = produceTwoHalves(array, middleIndex, targetNumber);
-  if (targetNumber <= firstHalf[firstHalf.length - 1]) {
-    binarySearch(firstHalf, targetNumber);
+
+  if (array[middleIndex] < targetNumber) {
+    startIndex = middleIndex + 1;
   } else {
-    binarySearch(secondHalf, targetNumber);
+    endIndex = middleIndex;
   }
+  return binarySearch(array.slice(startIndex, endIndex + 1), targetNumber);
 };
 
-const produceTwoHalves = (array, middleIndex, targetNumber) => {
-  if (array[middleIndex] === targetNumber) return middleIndex;
-  const firstHalf = getFirstHalf(middleIndex, array);
-  const secondHalf = getSecondHalf(middleIndex, array);
-  return { firstHalf, secondHalf };
-};
-
-const getFirstHalf = (middleIndex, array) => {
-  const firstHalf = [];
-  for (let i = 0; i < middleIndex; i++) {
-    firstHalf.push(array[i]);
-  }
-  return firstHalf;
-};
-
-const getSecondHalf = (middleIndex, array) => {
-  const secondHalf = [];
-  for (let i = middleIndex; i < array.length; i++) {
-    secondHalf.push(array[i]);
-  }
-  return secondHalf;
-}
+// [ 5, 6, 7, 8, 9 ];
--- kata-with-jest/src/Academind/binarySearch.test.js
+++ kata-with-jest/src/Academind/binarySearch.test.js
@@ -1,7 +1,7 @@
 import { binarySearch } from './binarySearch';
 
 describe('binarySearch()', ()=>{
-  describe('GIVEN: a sorted array and a target number', ()=>{
+  describe('GIVEN: a sorted array with an odd number of elements and a target number', ()=>{
     describe('WHEN: the target number is in the middle of the array', ()=>{
       it('THEN: It returns the index of the target number', ()=>{
         const array = [ 5, 6, 7, 8, 9 ];
@@ -15,6 +15,28 @@ describe('binarySearch()', ()=>{
     describe('WHEN: the target number is NOT in the middle of the array', ()=>{
       it('THEN: It returns the index of the target number', ()=>{
         const array = [ 5, 6, 7, 8, 9 ];
+        const targetNumber = 8;
+
+        const result = binarySearch(array, targetNumber);
+
+        expect(result).toEqual(3);
+      });
+    });
+  });
+  describe('GIVEN: a sorted array with an even number of elements and a target number', ()=>{
+    describe('WHEN: the target number is in the middle of the array', ()=>{
+      it('THEN: It returns the index of the target number', ()=>{
+        const array = [ 5, 6, 7, 8, 9, 10 ];
+        const targetNumber = 7;
+
+        const result = binarySearch(array, targetNumber);
+
+        expect(result).toEqual(2);
+      });
+    });
+    describe('WHEN: the target number is NOT in the middle of the array', ()=>{
+      it('THEN: It returns the index of the target number', ()=>{
+        const array = [ 5, 6, 7, 8, 9, 10 ];
         const targetNumber = 8;
 
         const result = binarySearch(array, targetNumber);
