f887e64a50abca4959559c493ac1feeceb1c3f05
Alex Gochenour <alexgochenour@gmail.com>
2021-12-20 23:15:56 -0500

    Got the unhappy paths down.

--- src/codewars/sudokuValidator.js
+++ src/codewars/sudokuValidator.js
@@ -1,7 +1,79 @@
 /**
  * @param sudokuBoard: A 2D array of numbers.
- * @return {boolean} where true represents a valid solution, i.e. the
+ * @return {boolean} where true represents a valid solution.
  * */
-export function determineValidity(sudokuBoard){
 
+export const determineValidity = (sudokuBoard) => {
+  if (
+      containsAZero(sudokuBoard)
+    | hasRepsInRows(sudokuBoard)
+    | hasRepsInCols(sudokuBoard)
+  ) return false;
+};
+
+const hasRepsInCols = (sudokuBoard) => {
+  let hasReps = false;
+  for (let i = 0; i < sudokuBoard.length - 1; i++){
+    let columnValues = [];
+    for (let j = 0; j < sudokuBoard.length; j++){
+      if (columnValues.includes(sudokuBoard[j][i])) {
+        return true;
+      } else {
+        columnValues.push(sudokuBoard[j][i]);
+      }
+    };
+  };
+  return hasReps;
+};
+
+const hasRepsInRows = (sudokuBoard) => {
+  let hasReps = false;
+  for (let i = 0; i < sudokuBoard.length - 1; i++){
+    const row = sudokuBoard[i];
+    if (thereAreReps(row)) {
+      hasReps = true;
+      break;
+    }
+  }
+  return hasReps;
+};
+
+const thereAreReps = (arrayOfNums) => {
+  let hasReps = false;
+  let values = [];
+  for (let i = 0; i < arrayOfNums.length; i++){
+    if (values.includes(arrayOfNums[i])) {
+      hasReps = true;
+      break;
+    } else {
+      values.push(arrayOfNums[i]);
+    }
+  };
+  return hasReps;
+};
+
+const containsAZero = (sudokuBoard) => {
+  let hasAZero = false;
+  for (let i = 0; i < sudokuBoard.length - 1; i++) {
+    if (innerArrayContainsZero(sudokuBoard[i])) {
+      hasAZero = true;
+      break;
+    }
+  }
+  return hasAZero;
+};
+
+const innerArrayContainsZero = (innerArray) => {
+  let containsZero = false;
+  for (let i = 0; i < innerArray.length - 1; i++){
+    if (!isNotZero(innerArray[i])) {
+      containsZero = true;
+      break;
+    }
+  };
+  return containsZero;
 }
+
+const isNotZero = (arrayValue) => arrayValue !== 0;
+
+// TODO: Explore the Array.prototype.every() function.
--- src/codewars/sudokuValidator.test.js
+++ src/codewars/sudokuValidator.test.js
@@ -15,8 +15,18 @@ const validSolution = [
   [2, 8, 7, 4, 1, 9, 6, 3, 5],
   [3, 4, 5, 2, 8, 6, 1, 7, 9]
 ];
-
-const invalidSolution = [
+const invalidFromRepetitionInCol = [
+  [5, 3, 4, 6, 7, 8, 9, 2, 1],
+  [6, 7, 2, 9, 5, 3, 4, 8, 1],
+  [9, 8, 3, 4, 2, 5, 6, 7, 1],
+  [8, 5, 9, 7, 6, 4, 2, 3, 1],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 3, 9, 2, 4, 8, 5, 6, 1],
+  [9, 6, 5, 3, 7, 2, 8, 4, 1],
+  [2, 8, 7, 4, 9, 6, 3, 5, 1],
+  [3, 4, 5, 2, 8, 6, 7, 9, 1],
+];
+const invalidSolutionWithZeroes = [
   [5, 3, 4, 6, 7, 8, 9, 1, 2],
   [6, 7, 2, 1, 9, 0, 3, 4, 8],
   [1, 0, 0, 3, 4, 2, 5, 6, 0],
@@ -27,14 +37,48 @@ const invalidSolution = [
   [2, 8, 7, 4, 1, 9, 6, 3, 5],
   [3, 0, 0, 4, 8, 1, 1, 7, 9]
 ];
+const invalidFromRepetitionInRow = [
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 3, 3, 4, 8],
+  [1, 3, 3, 3, 4, 2, 5, 6, 3],
+  [8, 5, 9, 7, 6, 1, 2, 2, 2],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 2, 1, 5, 3, 7, 2, 1, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 2, 2, 4, 8, 1, 1, 7, 9]
+];
 
 describe('determineValidity()', ()=>{
   describe('GIVEN: An invalid solution', ()=>{
     describe('WHEN: A solution with zeroes somewhere on the board', ()=>{
       it('THEN: It returns false.', ()=>{
-        const result = determineValidity(invalidSolution);
+        const result = determineValidity(invalidSolutionWithZeroes);
 
-        expect(result).toBeFalsy();
+        expect(result).toEqual(false);
+      });
+    });
+    describe('WHEN: A solution is invalid due to repetitions in at least one row', ()=>{
+      it('THEN: It returns false.', ()=>{
+        const result = determineValidity(invalidFromRepetitionInRow);
+
+        expect(result).toEqual(false);
+      });
+    });
+    describe('WHEN: A solution is invalid due to repetitions in at least one column', ()=>{
+      it('THEN: It returns false.', ()=>{
+        const result = determineValidity(invalidFromRepetitionInCol);
+
+        expect(result).toEqual(false);
+      });
+    });
+  });
+  describe('GIVEN: A valid solution', ()=>{
+    describe('WHEN: A solution with NO zeroes somewhere on the board', ()=>{
+      it('THEN: It returns false.', ()=>{
+        const result = determineValidity(validSolution);
+
+        expect(result).toEqual(true);
       });
     });
   });
