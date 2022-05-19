966aa1170b719d28c46d9a8d6f3314be8d7de3c3
Alex Gochenour <alexgochenour@gmail.com>
2022-05-18 22:42:17 -0400

    feat: added a third parameter, annualContribution.

--- src/investmentCalculator.js
+++ src/investmentCalculator.js
@@ -1,6 +1,27 @@
-export const investmentCalculator = (startingValueUSD, interestRate) => {
-  let balance = startingValueUSD;
+export const investmentCalculator = (startingValueUSD, interestRate, annualContribution) => {
   const processedRate = processInterestRate(interestRate);
+  return (!annualContribution) 
+    ? processWithoutAnnualContribution(startingValueUSD, processedRate)
+    : processWithAnnualContribution(startingValueUSD, processedRate, annualContribution);
+};
+
+const processWithAnnualContribution = (startingValueUSD, processedRate, annualContribution) => {
+  let balance = startingValueUSD;
+  const tableRows = [];
+  for (let q = 0; q < 4; q++) {
+    if (q === 0) {
+      tableRows.push([startingValueUSD, 0]);
+    }
+    balance *= processedRate;
+    balance += annualContribution;
+    tableRows.push([Math.trunc(balance), q + 1]);
+  }
+  console.table(tableRows);
+  return tableRows;
+};
+
+const processWithoutAnnualContribution = (startingValueUSD, processedRate) => {
+  let balance = startingValueUSD;
   const tableRows = [];
   for (let q = 0; q < 4; q++) {
     if (q === 0) {
@@ -11,6 +32,6 @@ export const investmentCalculator = (startingValueUSD, interestRate) => {
   }
   console.table(tableRows);
   return tableRows;
-};
+}
 
 const processInterestRate = (interestRate) => (1 + (interestRate / 100));
--- src/investmentCalculator.test.js
+++ src/investmentCalculator.test.js
@@ -1,21 +1,41 @@
 import { investmentCalculator } from './investmentCalculator';
 
-describe('GIVEN: a starting value and an interest rate,', ()=>{
-  test('THEN: it logs a table where each row contains number of years and balance.', ()=>{
-    const spy = jest.spyOn(console, 'table');
-    const startingValueUSD = 73_000;
-    const interestRate = 2.7;
-    const expectedResult = [
-      [73000, 0],
-      [74971, 1],
-      [76995, 2],
-      [79074, 3],
-      [81209, 4],
-    ];
+const spy = jest.spyOn(console, 'table');
+const startingValueUSD = 73_000;
+const interestRate = 2.7;
 
-    const result = investmentCalculator(startingValueUSD, interestRate);
+describe('GIVEN: investmentCalculator()', ()=>{
+  describe('WHEN: a starting value and an interest rate,', ()=>{
+    test('THEN: it logs a table where each row contains number of years and balance.', ()=>{
+      const expectedResult = [
+        [73_000, 0],
+        [74_971, 1],
+        [76_995, 2],
+        [79_074, 3],
+        [81_209, 4],
+      ];
 
-    // expect(spy).toBeCalledWith(expectedResult);
-    expect(result).toEqual(expectedResult);
+      const result = investmentCalculator(startingValueUSD, interestRate);
+
+      expect(result).toEqual(expectedResult);
+      expect(spy).toBeCalledWith(expectedResult);
+    });
+  });
+  describe('WHEN: a starting value, an interest rate, and an amount contributed annually,', ()=>{
+    test('THEN: it logs a table where each row contains number of years and balance.', ()=>{
+      const annualContribution = 48_000;
+      const expectedResult = [
+        [73_000, 0],
+        [122_971, 1],
+        [174_291, 2],
+        [226_997, 3],
+        [281_126, 4],
+      ];
+
+      const result = investmentCalculator(startingValueUSD, interestRate, annualContribution);
+
+      expect(result).toEqual(expectedResult);
+      expect(spy).toBeCalledWith(expectedResult);
+    });
   });
 });
