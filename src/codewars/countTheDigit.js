f9f7418160cbacee8b481552d14b3307cb0a33e9
Alex Gochenour <alexgochenour@gmail.com>
2022-06-12 00:09:08 -0400

    refactor

--- src/codewars/countTheDigit.js
+++ src/codewars/countTheDigit.js
@@ -1,17 +1,21 @@
 export const nbDig = (n, d) => {
+  const digit = d.toString();
+  const squares = getSquares(n);
+  return squares
+    .filter(square => square.split('').includes(digit))
+    .map(square => getDigitCount(square, digit))
+    .reduce((previous, current) => previous + current, 0);
+};
+
+const getDigitCount = (squareNumber, digit) => {
+  let digitCount = 0;
+  squareNumber.split('').forEach(char => (char === digit && digitCount++));
+  return digitCount;
+};
+const getSquares = (n) => {
   const squares = [];
   for (let i = 0; i < n+1; i++){
     squares.push(Math.pow(i, 2).toString());
   }
-  const result = squares.filter(square => {
-    const asArray = square.split('');
-    return asArray.includes(d.toString());
-  })
-  .map(square => {
-    let numberOfTargetDigit = 0;
-    square.split('').forEach(char => (char === d.toString() && numberOfTargetDigit++));
-    return numberOfTargetDigit; 
-  })
-  .reduce((previous, current) => previous + current, 0);
-  return result;
+  return squares;
 };
