c8c22251dda9388e4dc3dbf2a8f7ea6f780865af
Alex Gochenour <alexgochenour@gmail.com>
2022-06-13 19:24:03 -0400

    domainName kata from Codewars, including the edge case that was not given in the example.

--- src/codewars/domainName.js
+++ src/codewars/domainName.js
@@ -2,16 +2,14 @@
 
 export function domainName(url){
   const startingSegments = ['http://www.', 'https://www.', 'https://', 'http://', 'www.'];
-  // const beginningOfUrl = startingSegments.filter(segment => url.includes(segment));
   let beginningOfUrl;
   for (const segment of startingSegments) {
     if (url.includes(segment)) {
-      console.log('segment:   ', segment);
       beginningOfUrl = segment;
       break;
     }
   }
-  return url
-    .split(beginningOfUrl)[1]
-    .split('.')[0];
+  return (!beginningOfUrl)
+    ? url.split('.')[0]
+    : url.split(beginningOfUrl)[1].split('.')[0];
 }
