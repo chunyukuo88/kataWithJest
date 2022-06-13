269b86c3560d9247681d086895a4ad646d7e3776
Alex Gochenour <alexgochenour@gmail.com>
2022-06-13 19:27:57 -0400

    refactor domainName

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,13 @@ K
 src/investmentCalculator.js,5/7/576811f7f527ca8c90115750ce6c851a73e351cf
 P
  src/investmentCalculator.test.js,1/6/1626dc3b200556aebfb11d863fc19dc02e2576a2
+R
+"src/codewars/countTheDigit.test.js,1/a/1a088aa53e0acfe7635ae7f896103bb28a6a2778
+M
+src/codewars/countTheDigit.js,a/3/a35cecce7f146ad17674cefcf5cfd6b1e5bfbb41
+S
+#src/codewars/lifePathNumber.test.js,e/9/e9e78aec58119b17088492b843fce8e51a6350b7
+N
+src/codewars/lifePathNumber.js,a/d/ad214d2579e5e360bf7388236cb0a4c95d81aa2a
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
--- src/codewars/domainName.js
+++ src/codewars/domainName.js
@@ -1,15 +1,25 @@
 // https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
 
+/** My original: */
+// export function domainName(url){
+//   const startingSegments = ['http://www.', 'https://www.', 'https://', 'http://', 'www.'];
+//   let beginningOfUrl;
+//   for (const segment of startingSegments) {
+//     if (url.includes(segment)) {
+//       beginningOfUrl = segment;
+//       break;
+//     }
+//   }
+//   return (!beginningOfUrl)
+//     ? url.split('.')[0]
+//     : url.split(beginningOfUrl)[1].split('.')[0];
+// }
+
+/** Ultra-concise alternative: */
 export function domainName(url){
-  const startingSegments = ['http://www.', 'https://www.', 'https://', 'http://', 'www.'];
-  let beginningOfUrl;
-  for (const segment of startingSegments) {
-    if (url.includes(segment)) {
-      beginningOfUrl = segment;
-      break;
-    }
-  }
-  return (!beginningOfUrl)
-    ? url.split('.')[0]
-    : url.split(beginningOfUrl)[1].split('.')[0];
-}
+  return url
+    .replace('https://', '')
+    .replace('http://', '')
+    .replace('www.', '')
+    .split('.')[0];
+};
