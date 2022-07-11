10a1b67374be58463c75f9e5e2ca8ae30818da48
Alex Gochenour <alexgochenour@gmail.com>
2022-07-11 12:41:11 -0400

    refactored generateMockRequests.js

--- src/generateDynamicDates.test.js
+++ src/generateDynamicDates.test.js
@@ -77,12 +77,136 @@ describe('GIVEN: ', () => {
   });
   describe('WHEN: The current month is November', () => {
     it('THEN: It returns dates for December and January.', () => {
-      //
+      jest.setSystemTime(1668351600000); // === '2022-11-13', 10:00 a.m.
+      const expectedResult = [
+        {
+          month: 'December',
+          requestedDays: 3,
+          year: 2022,
+          days: [
+            {
+              day: 1,
+              month: 'December',
+              year: 2022,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+            {
+              day: 2,
+              month: 'December',
+              year: 2022,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+            {
+              day: 3,
+              month: 'December',
+              year: 2022,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+          ],
+        },
+        {
+          month: 'January',
+          requestedDays: 3,
+          year: 2023,
+          days: [
+            {
+              day: 1,
+              month: 'January',
+              year: 2023,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+            {
+              day: 2,
+              month: 'January',
+              year: 2023,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+            {
+              day: 3,
+              month: 'January',
+              year: 2023,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+          ],
+        },
+      ];
+
+      const result = generateMockRequests();
+
+      expect(result).toEqual(expectedResult);
     });
   });
   describe('WHEN: The current month is December', () => {
     it('THEN: It returns dates for January and February.', () => {
-      //
+      jest.setSystemTime(1671980400000); // === '2022-12-25', 10:00 a.m.
+      const expectedResult = [
+        {
+          month: 'January',
+          requestedDays: 3,
+          year: 2023,
+          days: [
+            {
+              day: 1,
+              month: 'January',
+              year: 2023,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+            {
+              day: 2,
+              month: 'January',
+              year: 2023,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+            {
+              day: 3,
+              month: 'January',
+              year: 2023,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+          ],
+        },
+        {
+          month: 'February',
+          requestedDays: 3,
+          year: 2023,
+          days: [
+            {
+              day: 1,
+              month: 'February',
+              year: 2023,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+            {
+              day: 2,
+              month: 'February',
+              year: 2023,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+            {
+              day: 3,
+              month: 'February',
+              year: 2023,
+              time: '10:00AM',
+              status: 'Requested',
+            },
+          ],
+        },
+      ];
+
+      const result = generateMockRequests();
+
+      expect(result).toEqual(expectedResult);
     });
   });
 });
--- src/generateMockRequests.js
+++ src/generateMockRequests.js
@@ -17,33 +17,36 @@ export const generateMockRequests = () => {
   const date = new Date();
   const currentMonth = date.getMonth();
 
-  if (currentMonth <= 9) {
-    const lexicalMonthCurrent = months[currentMonth + 1];
-    const lexicalMonthNext = months[currentMonth + 2];
+  let lexicalMonthCurrent = months[currentMonth + 1];
+  let lexicalMonthNext = months[currentMonth + 2];
+
+  if (currentMonth === 10) {
+    lexicalMonthCurrent = months[11];
+    lexicalMonthNext = months[0]
     return [
       {
         month: lexicalMonthCurrent,
         requestedDays: 3,
-        year: 2022,
+        year: date.getFullYear(),
         days: [
           {
             day: 1,
             month: lexicalMonthCurrent,
-            year: 2022,
+            year: date.getFullYear(),
             time: '10:00AM',
             status: 'Requested',
           },
           {
             day: 2,
             month: lexicalMonthCurrent,
-            year: 2022,
+            year: date.getFullYear(),
             time: '10:00AM',
             status: 'Requested',
           },
           {
             day: 3,
             month: lexicalMonthCurrent,
-            year: 2022,
+            year: date.getFullYear(),
             time: '10:00AM',
             status: 'Requested',
           },
@@ -52,26 +55,26 @@ export const generateMockRequests = () => {
       {
         month: lexicalMonthNext,
         requestedDays: 3,
-        year: 2022,
+        year: date.getFullYear() + 1,
         days: [
           {
             day: 1,
             month: lexicalMonthNext,
-            year: 2022,
+            year: date.getFullYear() + 1,
             time: '10:00AM',
             status: 'Requested',
           },
           {
             day: 2,
             month: lexicalMonthNext,
-            year: 2022,
+            year: date.getFullYear() + 1,
             time: '10:00AM',
             status: 'Requested',
 
           },
           {
             day: 3,
-            year: 2022,
+            year: date.getFullYear() + 1,
             month: lexicalMonthNext,
             time: '10:00AM',
             status: 'Requested',
@@ -80,5 +83,126 @@ export const generateMockRequests = () => {
       },
     ];
   }
+  if (currentMonth === 11) {
+    lexicalMonthCurrent = months[0];
+    lexicalMonthNext = months[1]
+    return [
+      {
+        month: lexicalMonthCurrent,
+        requestedDays: 3,
+        year: date.getFullYear() + 1,
+        days: [
+          {
+            day: 1,
+            month: lexicalMonthCurrent,
+            year: date.getFullYear() + 1,
+            time: '10:00AM',
+            status: 'Requested',
+          },
+          {
+            day: 2,
+            month: lexicalMonthCurrent,
+            year: date.getFullYear() + 1,
+            time: '10:00AM',
+            status: 'Requested',
+          },
+          {
+            day: 3,
+            month: lexicalMonthCurrent,
+            year: date.getFullYear() + 1,
+            time: '10:00AM',
+            status: 'Requested',
+          },
+        ],
+      },
+      {
+        month: lexicalMonthNext,
+        requestedDays: 3,
+        year: date.getFullYear() + 1,
+        days: [
+          {
+            day: 1,
+            month: lexicalMonthNext,
+            year: date.getFullYear() + 1,
+            time: '10:00AM',
+            status: 'Requested',
+          },
+          {
+            day: 2,
+            month: lexicalMonthNext,
+            year: date.getFullYear() + 1,
+            time: '10:00AM',
+            status: 'Requested',
 
+          },
+          {
+            day: 3,
+            year: date.getFullYear() + 1,
+            month: lexicalMonthNext,
+            time: '10:00AM',
+            status: 'Requested',
+          },
+        ],
+      },
+    ];
+  }
+  return [
+    {
+      month: lexicalMonthCurrent,
+      requestedDays: 3,
+      year: date.getFullYear(),
+      days: [
+        {
+          day: 1,
+          month: lexicalMonthCurrent,
+          year: date.getFullYear(),
+          time: '10:00AM',
+          status: 'Requested',
+        },
+        {
+          day: 2,
+          month: lexicalMonthCurrent,
+          year: date.getFullYear(),
+          time: '10:00AM',
+          status: 'Requested',
+        },
+        {
+          day: 3,
+          month: lexicalMonthCurrent,
+          year: date.getFullYear(),
+          time: '10:00AM',
+          status: 'Requested',
+        },
+      ],
+    },
+    {
+      month: lexicalMonthNext,
+      requestedDays: 3,
+      year: date.getFullYear(),
+      days: [
+        {
+          day: 1,
+          month: lexicalMonthNext,
+          year: date.getFullYear(),
+          time: '10:00AM',
+          status: 'Requested',
+        },
+        {
+          day: 2,
+          month: lexicalMonthNext,
+          year: date.getFullYear(),
+          time: '10:00AM',
+          status: 'Requested',
+
+        },
+        {
+          day: 3,
+          year: date.getFullYear(),
+          month: lexicalMonthNext,
+          time: '10:00AM',
+          status: 'Requested',
+        },
+      ],
+    },
+  ];
 };
