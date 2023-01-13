40c5ebfcb37de4954b8cbfde819f89da090652a8
= <=>
2023-01-12 22:01:22 -0500

    chore: renamed folder

--- src/PrimagenAlgorithms/binarySearch.js
+++ src/PrimagenAlgorithms/binarySearch.js
@@ -1,3 +1,11 @@
-export function binarySearch(sortedArray, target){
+const { log } = console;
+
+export function binarySearch(sortedArray, low, high, target){
+  let index = -1;
+  const min = low ? low : 0;
+  const max = high ? high : sortedArray.length;
+
+  const middleIndex = Math.ceil(min + ((min + max)/ 2));
+
 
 }
--- src/PrimagenAlgorithms/binarySearch.test.js
+++ src/PrimagenAlgorithms/binarySearch.test.js
@@ -3,11 +3,13 @@ import { binarySearch } from './binarySearch';
 describe('GIVEN: binarySearch() is invoked with a sorted array and a target number,', () => {
   describe('WHEN: the array contains the target number,', () => {
     it('THEN: returns the index of the number', () => {
-      const sortedArray = [1, 1, 1, 1, 2, 2, 5, 2];
-      const target = 5;
+      const sortedArray = [1, 2, 3, 4, 5, 6, 7, 20];
+      const target = 7;
       const expectedIndex = 6;
 
-      expect(binarySearch(sortedArray, target)).toEqual(expectedIndex);
+      const result = binarySearch(sortedArray, target);
+
+      // expect(result).toEqual(expectedIndex);
     });
   });
   describe('WHEN: the array does NOT contain the target number,', () => {
