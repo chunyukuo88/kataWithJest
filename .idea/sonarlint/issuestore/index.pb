632f42b4305ab9699d61b80f6f5f2383f861d4b7
Alex Gochenour <alex.gochenour@offorhealth.com>
2023-01-22 01:04:28 -0500

    WIP: Quicksort is hard.

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,27 +0,0 @@
-
-K
-src/investmentCalculator.js,5/7/576811f7f527ca8c90115750ce6c851a73e351cf
-P
- src/investmentCalculator.test.js,1/6/1626dc3b200556aebfb11d863fc19dc02e2576a2
-R
-"src/codewars/countTheDigit.test.js,1/a/1a088aa53e0acfe7635ae7f896103bb28a6a2778
-M
-src/codewars/countTheDigit.js,a/3/a35cecce7f146ad17674cefcf5cfd6b1e5bfbb41
-S
-#src/codewars/lifePathNumber.test.js,e/9/e9e78aec58119b17088492b843fce8e51a6350b7
-N
-src/codewars/lifePathNumber.js,a/d/ad214d2579e5e360bf7388236cb0a4c95d81aa2a
-J
-src/codewars/domainName.js,b/0/b0e4b650ef2e18da6df47940066473a1a4ed4e7a
-O
-src/codewars/domainName.test.js,b/f/bf741d95215b5b588a9a463ec608e6c9a8073682
-P
- src/codewars/hashtagGenerator.js,5/3/532f498545f3a2c0efcd2ac339265f3f392b4009
-U
-%src/codewars/hashtagGenerator.test.js,d/0/d0916d96e1be803ea678dba08bbc7cf6e7e344aa
-]
--src/algorithmsCourseFEM/linearBinaryBubble.js,f/3/f32c220a887b18031569ba585d3dee555a4531e2
-b
-2src/algorithmsCourseAcademind/binarySearch.test.js,a/7/a7a48a8f4230147a5a6a5e027f3576e7b6c58ea4
-]
--src/algorithmsCourseAcademind/binarySearch.js,c/9/c9bb1627c9cce0527a7a830b2ba84b9920f8a2f8
--- src/algorithmsCoursePrimagen/quickSort.js
+++ src/algorithmsCoursePrimagen/quickSort.js
@@ -1,3 +1,58 @@
-export function quickSort(arrayOfInts){
-  //
+function swapPositions(first, last, arr) {
+  const updated = [...arr];
+  updated[0] = last;
+  updated[updated.length - 1] = first;
+  return updated;
+}
+
+function swapAdjacentNumbers(arr) {
+  const updated = [];
+  updated[0] = arr[1];
+  updated[1] = arr[0];
+  return updated;
+}
+
+function processSmallArr(arr) {
+  if (arr.length <= 1) return arr;
+  return (arr[0] > arr[1])
+    ? swapAdjacentNumbers(arr)
+    : arr;
+}
+
+export function quickSort(arrayOfInts) {
+  if (arrayOfInts.length <= 2) {
+    return processSmallArr(arrayOfInts);
+  }
+  const pivotIndex = Math.floor(arrayOfInts.length / 2);
+  let updated = [...arrayOfInts];
+  let walkRight = 0;
+  let walkLeft = updated.length - 1;
+  let first = updated[walkRight];
+  let middle = updated[pivotIndex];
+  let last = updated[walkLeft];
+  while (first !== middle && last !== middle) {
+    if (first > middle && last < middle) {
+      updated = swapPositions(first, last, updated)
+      walkRight++;
+      walkLeft--;
+      first = updated[walkRight];
+      last = updated[walkLeft];
+    } else if (first > middle && !(last < middle)) {
+      walkLeft--;
+      last = updated[walkLeft];
+    } else if (!(first > middle) && last < middle) {
+      walkRight++;
+      first = updated[walkRight];
+    } else {
+      walkRight++;
+      walkLeft--;
+      first = updated[walkRight];
+      last = updated[walkLeft];
+    }
+  }
+  const firstHalf = updated.slice(0, pivotIndex);
+  const secondHalf = updated.slice(pivotIndex, updated.length);
+  const firstSorted = quickSort(firstHalf);
+  const secondSorted = quickSort(secondHalf);
+  return firstSorted.concat(secondSorted);
 }
